52daa4ee2ef14debba54ff1e7ca9f6d169e12673
Efren Diaz <efren088@gmail.com>
2018-05-01 19:18:39 -0400

    Organize files a bit better

---
+++ lib/meeting.rb
@@ -0,0 +1,21 @@
+class Meeting < Base
+  attr_accessor :title, :duration, :begin_time
+
+  def to_s
+    duration.nil? ? "#{begin_time_to_human} #{title}" : "#{begin_time_to_human} #{title} #{duration}min"
+  end
+
+  def begin_time_to_human
+    hour =  "%02d" % begin_time.to_i
+    minutes = decimal_to_minutes(begin_time.modulo(1))
+    if begin_time >= 9
+      "#{hour}:#{minutes}AM"
+    else
+      "#{hour}:#{minutes}PM"
+    end
+  end
+
+  def decimal_to_minutes(n)
+    "%02d" % (n * 60).to_i
+  end
+end
